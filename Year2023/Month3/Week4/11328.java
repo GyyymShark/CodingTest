6664c3d958b037e3187790e71354fadbf6953d52
the-groot <sangwoo0795@naver.com>
2023-03-27 13:59:46 +0900

    [BOJ] Strfry

--- Year2023/Month3/Week4/11328.java
+++ Year2023/Month3/Week4/11328.java
@@ -4,27 +4,33 @@ import java.util.*;
 
 class Main {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n;
-        n=sc.nextInt();
-        sc.nextLine();11328 java
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
 
+       char[] alp=new char[26];
 
-        for(int i=0; i<n; i++) {
-            String s1 = sc.next();
-            String s2 = sc.next();
-            char[] charS1 = s1.toCharArray();
-            char[] charS2 = s2.toCharArray();
-            Arrays.sort(charS2);
-            Arrays.sort(charS1);
+       for(int i=0; i<n; i++){
+           bool possible=true;
+           String s1=sc.next();
+           String s2=sc.next();
 
-            s1 = new String(charS1);
-            s2 = new String(charS2);
-            if(s1.equals(s2))
-                System.out.println("Possible");
-            else
-                System.out.println("Impossible");
-        }
+           for(char x : s1){
+               alp[s1-'a']++;
+           }
+           for(char x : s2){
+               alp[s2-'a']--;
+           }
+
+           for(int j=0; j<26; j++){
+               if(alp[j]!=0){
+                   System.out.println("Impossible");
+                   possible=false;
+                   break;
+               }
+           }
+           if(possible)
+               System.out.println("Possible");
+       }
 
     }
 }
