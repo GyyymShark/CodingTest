c4d9840d9b3e6cb2d5fcd0f322b0e59efa2719ba
Rim Sangwoo <sangwoo0795@naver.com>
2023-07-03 15:54:33 +0900

    [Bronze V] Title: ??!, Time: 132 ms, Memory: 14392 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+       BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st=new StringTokenizer(br.readLine());
+
+       String str=st.nextToken();
+       str=str+"??!";
+       System.out.println(str);
+
+
+    }
+}
