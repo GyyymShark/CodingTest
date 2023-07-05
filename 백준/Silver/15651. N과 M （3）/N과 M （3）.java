dcfdd729eb286c1f59f20bb7268e5c7837289b72
Rim Sangwoo <sangwoo0795@naver.com>
2023-07-05 09:33:25 +0900

    [Silver III] Title: N과 M (3), Time: 596 ms, Memory: 208100 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    static int n,m;
+    static boolean[] visited=new boolean[10];
+    static int[] arr=new int[10];
+    static StringBuilder sb=new StringBuilder();
+    public static void main(String[] args)throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        n=Integer.parseInt(st.nextToken());
+        m=Integer.parseInt(st.nextToken());
+        func(0);
+        System.out.println(sb);
+    }
+
+    static void func(int cur){
+        if(cur==m){
+            for(int i=0; i<m; i++){
+                sb.append(arr[i]+" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i=1; i<=n; i++){
+            arr[cur]=i;
+            func(cur+1);
+        }
+    }
+}
