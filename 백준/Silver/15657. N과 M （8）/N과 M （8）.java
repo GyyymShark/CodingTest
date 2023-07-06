23e1d04a6e83ec98a7d01484c3f670c6ec3817d7
Rim Sangwoo <sangwoo0795@naver.com>
2023-07-06 10:04:59 +0900

    [Silver III] Title: N과 M (8), Time: 236 ms, Memory: 21276 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    static int n,m;
+    static int[] arr=new int[10];
+    static int[] board;
+    static StringBuilder sb=new StringBuilder();
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        n=Integer.parseInt(st.nextToken());
+        m=Integer.parseInt(st.nextToken());
+        st=new StringTokenizer(br.readLine());
+        board=new int[n];
+        for(int i=0; i<n; i++){
+            board[i]=Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(board);
+        func(0,0,0);
+        System.out.println(sb);
+
+    }
+    static void func(int depth, int idx, int cur){
+       if(depth==m){
+           for(int i=0; i<m; i++){
+               sb.append(arr[i]+" ");
+           }
+           sb.append("\n");
+           return;
+       }
+
+       for(int i=idx; i<n; i++){
+           arr[cur]=board[i];
+           func(depth+1, i, cur+1);
+       }
+    }
+}
