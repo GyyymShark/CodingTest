a5ff1ee6bd8de977794d82b582d2a114bcc6eb5b
Rim Sangwoo <sangwoo0795@naver.com>
2023-07-06 12:26:13 +0900

    [Silver II] Title: N과 M (12), Time: 176 ms, Memory: 15952 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    static int n,m;
+    static int[] board;
+    static int[] arr=new int[10];
+    static StringBuilder sb=new StringBuilder();
+    static boolean[] visited=new boolean[10];
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        n=Integer.parseInt(st.nextToken());
+        m=Integer.parseInt(st.nextToken());
+
+        board=new int[n];
+        st=new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            board[i]=Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(board);
+
+        func(0,0);
+        System.out.print(sb);
+
+    }
+    static void func(int depth, int idx){
+        if(depth==m){
+            for(int i=0; i<m; i++){
+                sb.append(arr[i]+" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int before=0;
+        for(int i=idx; i<n; i++){
+            if(before!=board[i]){
+                arr[depth]=board[i];
+                before=board[i];
+                func(depth+1, i);
+            }
+        }
+
+    }
+}
