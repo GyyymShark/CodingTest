2e4b1fbfa9d92d10d93ae6b10345b9c3bedf9a2f
Rim Sangwoo <sangwoo0795@naver.com>
2023-07-13 12:30:11 +0900

    [Gold II] Title: 2048 (Easy), Time: 300 ms, Memory: 33952 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -1,83 +1,162 @@
 import java.util.*;
 import java.io.*;
 
+
 class Main{
-    static int n;
+    static int n,max=0;
     static int[][] board1=new int[21][21];
     static int[][] board2=new int[21][21];
-
+    //기울이기와
+    //방향별 브루투포스
     public static void main(String[] args)throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(br.readLine());
+
         n=Integer.parseInt(st.nextToken());
+
 
         for(int i=0; i<n; i++){
             st=new StringTokenizer(br.readLine());
-            for(int j=0; j<n; j++){
+            for(int j=0; j<n; j++)
                 board1[i][j]=Integer.parseInt(st.nextToken());
-            }
         }
 
-        int mx=0;
+
         for(int tmp=0; tmp<1024; tmp++){
             for(int i=0; i<n; i++)
                 for(int j=0; j<n; j++)
                     board2[i][j]=board1[i][j];
-            int brute=tmp;
 
+            int brute=tmp;
             for(int i=0; i<5; i++){
                 int dir=brute%4;
                 brute/=4;
-                tilt(dir);
+                if(dir==0){     //상
+                    up();
+                }
+                else if(dir==1){    //우
+                    right();
+                }
+                else if(dir==2){    //하
+                    down();
+                }
+                else if(dir==3){    //좌
+                    left();
+                }
+
+
             }
+
             for(int i=0; i<n; i++){
                 for(int j=0; j<n; j++){
-                    mx=Math.max(mx,board2[i][j]);
+                    if(board2[i][j]>max)
+                        max=board2[i][j];
                 }
             }
+
         }
 
-        System.out.println(mx);
+
+        System.out.println(max);
 
     }
 
-    static void rotate(){   //board2를 시계방향으로 90도 회전하는 함수
-        int[][] tmp=new int[21][21];
 
-        for(int i=0; i<n; i++){
+    static void right(){
+        for(int i=0; i<n; i++){ //행
+
+            int[] tmp=new int[n];
+            int tmpidx=0;
+
+            for(int j=n-1; j>=0; j--){
+                if(board2[i][j]==0) continue;
+
+                if(tmp[tmpidx]==0) tmp[tmpidx]=board2[i][j];
+
+                else if(tmp[tmpidx]==board2[i][j]) tmp[tmpidx++]*=2;
+
+                else tmp[++tmpidx]=board2[i][j];
+            }
+
             for(int j=0; j<n; j++){
-                tmp[i][j]=board2[i][j];
+                board2[i][n-1-j]=tmp[j];
             }
         }
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                board2[i][j]=tmp[n-1-j][i];
-            }
-        }
-
-
     }
 
-    static void tilt(int dir){
-        for(int i=dir; i>=0; i--) rotate();
+    static void left(){
+        for(int i=0; i<n; i++){ //행
 
-        for(int i=0; i<n; i++){
-            int[] tilted=new int[21]; //board2[i]를 왼족으로 기울인 결과를 저장할 변수
-            int idx=0;  //tilted 배열에서 어디에 삽입해야 하는지 가리키는 변수
+            int[] tmp=new int[n];
+            int tmpidx=0;
 
             for(int j=0; j<n; j++){
                 if(board2[i][j]==0) continue;
-                if(tilted[idx]==0)  //삽입할 위치가 비어있을때
-                    tilted[idx]=board2[i][j];
-                else if(tilted[idx]==board2[i][j])
-                    tilted[idx++]*=2;
-                else    //다른 값을 갖는 블록이 충돌
-                    tilted[++idx]=board2[i][j];
+
+                if(tmp[tmpidx]==0) tmp[tmpidx]=board2[i][j];
+
+                else if(tmp[tmpidx]==board2[i][j]) tmp[tmpidx++]*=2;
+
+                else tmp[++tmpidx]=board2[i][j];
             }
+
             for(int j=0; j<n; j++){
-                board2[i][j]=tilted[j];
+                board2[i][j]=tmp[j];
             }
         }
+    }
+
+    static void down(){
+        for(int j=0; j<n; j++){ //열
+
+            int[] tmp=new int[n];
+            int tmpidx=0;
+
+            for(int i=n-1; i>=0; i--){
+                if(board2[i][j]==0) continue;
+
+                if(tmp[tmpidx]==0) tmp[tmpidx]=board2[i][j];
+
+                else if(tmp[tmpidx]==board2[i][j]) tmp[tmpidx++]*=2;
+
+                else tmp[++tmpidx]=board2[i][j];
+            }
+
+            for(int i=0; i<n; i++){
+                board2[n-1-i][j]=tmp[i];
+            }
+        }
+    }
+
+    static void up(){
+        for(int j=0; j<n; j++){ //열
+
+            int[] tmp=new int[n];
+            int tmpidx=0;
+
+            for(int i=0; i<n; i++){
+                if(board2[i][j]==0) continue;
+
+                if(tmp[tmpidx]==0) tmp[tmpidx]=board2[i][j];
+
+                else if(tmp[tmpidx]==board2[i][j]) tmp[tmpidx++]*=2;
+
+                else tmp[++tmpidx]=board2[i][j];
+            }
+
+            for(int i=0; i<n; i++){
+                board2[i][j]=tmp[i];
+            }
+        }
+    }
+
+    static void printBoard2(){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print(board2[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
