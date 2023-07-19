4c30d12440f326396cf2421ea8b132e9b58c3aae
Rim Sangwoo <sangwoo0795@naver.com>
2023-07-19 12:29:59 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 848 ms, Memory: 59444 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+
+    static class Point{
+        int x;
+        int y;
+        Point(int x, int y){
+            this.x=x;
+            this.y=y;
+        }
+        @Override
+        public String toString(){
+            return x+" "+y;
+        }
+    }
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int n=Integer.parseInt(st.nextToken());
+        Point[] pointList=new Point[n];
+        StringBuilder sb=new StringBuilder();
+
+        for(int i=0; i<n; i++){
+            st=new StringTokenizer(br.readLine());
+            int x=Integer.parseInt(st.nextToken());
+            int y=Integer.parseInt(st.nextToken());
+            pointList[i]=new Point(x,y);
+        }
+        Comparator<Point> comparator=new Comparator<Point>(){
+            @Override
+            public int compare(Point s1, Point s2){
+                if(s1.x==s2.x){
+                    return s1.y-s2.y;
+                }
+                return s1.x-s2.x;
+            }
+        };
+
+        Arrays.sort(pointList, comparator);
+
+        for(Point point : pointList){
+            sb.append(point+"\n");
+        }
+        System.out.println(sb);
+
+    }
+}
