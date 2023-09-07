369878951b131d130dd3f2e8d7557db4a8ed752a
Rim Sangwoo <sangwoo0795@naver.com>
2023-09-07 12:20:15 +0900

    [unrated] Title: 귤 고르기, Time: 50.43 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -1,74 +1,33 @@
 import java.util.*;
 
-
 class Solution {
-    
-    static class Orange implements Comparable<Orange>{
-        int num;
-        int count;
-        
-        public Orange(int num, int count){
-            this.num=num;
-            this.count=count;
-        }
-        
-        @Override
-        public int compareTo(Orange o){
-            return o.count-count;
-        }
-    }
-    
-    
     public int solution(int k, int[] tangerine) {
-        int answer = 1;
-        int kind=1;
-        List<Orange> orange=new ArrayList<>();
-        Arrays.sort(tangerine);
+        int answer = 0;
         
-        for(int i=1; i<tangerine.length; i++){
-            if(tangerine[i]!=tangerine[i-1]) kind++;
-        }
-        int[] num=new int[kind];
-        int[] count=new int[kind];
+        Map<Integer, Integer> map=new HashMap<>();
         
-        num[0]=tangerine[0];
-        count[0]++;
-        
-        int countIdx=0;
-        
-        for(int i=1; i<tangerine.length; i++){
-            if(tangerine[i]!=tangerine[i-1]){   //앞과 뒤가 다르면 새로 추가
-                countIdx++;
-                num[countIdx]=tangerine[i];
-                count[countIdx]++;
-            }
-            else{       //같으면 횟수 증가
-                count[countIdx]++;
-            }
-        }
-      
-        
-        for(int i=0; i<kind; i++){
-            orange.add(new Orange(num[i],count[i]));
+        for(Integer a : tangerine){
+            map.put(a, map.getOrDefault(a,0)+1);
         }
         
-        Collections.sort(orange);
-        int orangeIdx=0;
-        for(int i=k; i>0; i--){
-            if(orange.get(orangeIdx).count==0){
-                answer++;
-                orangeIdx++;
-            } 
-            Orange temp=orange.get(orangeIdx);
-            temp.count=temp.count-1;
-            orange.set(orangeIdx,temp);
+        List<Integer> key=new ArrayList<>(map.keySet());
+        Collections.sort(key, new Comparator<>(){
+            @Override
+            public int compare(Integer i1, Integer i2){
+                return map.get(i2)-map.get(i1);
+          }
+        });
+        
+        
+        for(Integer value : key){
+            k-=map.get(value);
+            
+            answer++;
+            
+            if(k<=0) break;
         }
         
-        
-        
-      
-      
-       
+            
         
         
         return answer;
