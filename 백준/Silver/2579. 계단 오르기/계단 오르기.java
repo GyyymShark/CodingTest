0674ab76a4f9241b5d9e9594be4884e8d514201e
Rim Sangwoo <sangwoo0795@naver.com>
2023-09-13 17:57:42 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        int[] s=new int[n+1];
+        int[][] d=new int[n+1][3];
+
+
+
+        for(int i=1; i<=n; i++){
+            s[i]=Integer.parseInt(br.readLine());
+        }
+        if(n==1) {
+            System.out.println(s[1]);
+            return;
+        }
+
+        d[1][1]=s[1]; d[1][2]=0;
+        d[2][1]=s[2]; d[2][2]=s[1]+s[2];
+
+
+        for(int i=3; i<=n; i++){
+            d[i][1]=Math.max(d[i-2][1], d[i-2][2])+s[i];
+            d[i][2]=d[i-1][1]+s[i];
+        }
+        System.out.println(Math.max(d[n][1],d[n][2]));
+
+
+    }
+}
