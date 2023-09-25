0261aec17f3f09244f327b7a28b7b0e7c3124f7e
Rim Sangwoo <sangwoo0795@naver.com>
2023-09-25 21:46:35 +0900

    [level 2] Title: 타겟 넘버, Time: 3.61 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,27 +1,33 @@
+import java.util.*;
 class Solution {
-    static int[] sign=new int[20];
-    static int answer=0;
-    public int solution(int[] numbers, int target) {
-        dfs(0, numbers.length,numbers,target);
-        return answer;
+    static class Point{
+        int idx;
+        int sum;
+        Point(int idx, int sum){
+            this.idx=idx;
+            this.sum=sum;
+        }
     }
+    static Queue<Point> q=new LinkedList<>();
     
-    static void dfs(int cur, int depth, int[] numbers, int target){
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+    
+        q.offer(new Point(0,0));
         
-        if(cur==depth){
-            int count=0;
-            for(int i=0; i<depth; i++){
-                count+=numbers[i]*sign[i];
+        while(!q.isEmpty()){
+            Point cur=q.poll();
+            if(cur.idx==numbers.length){
+                if(cur.sum==target) answer++;
             }
-            if(count==target) answer++;
             
-            return;
+            else{
+                q.offer(new Point(cur.idx+1, cur.sum+numbers[cur.idx]));
+                q.offer(new Point(cur.idx+1, cur.sum-numbers[cur.idx]));
+            }
         }
         
         
-        sign[cur]=1;
-        dfs(cur+1, depth, numbers, target);
-        sign[cur]=-1;
-        dfs(cur+1, depth, numbers, target);
+        return answer;
     }
 }
